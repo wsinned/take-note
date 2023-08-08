b77dfa9ac80c69852270c3da63f26727deb4d7e1
Dennis Woodruff <denniswoodruffuk@gmail.com>
2023-08-08 09:06:55 +0100

    Adds basic entry point for application

---
+++ src/take-note.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+
+from takenote.__main__ import main
+
+main()
---
+++ src/takenote/__main__.py
@@ -0,0 +1,6 @@
+
+def main():
+    print("Welcome to take-note-cli.")
+
+if __name__ == "__main__":
+    main()
