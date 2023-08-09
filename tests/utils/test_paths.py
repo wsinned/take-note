344f48172dec1f08c2a7d6a8eecc2edf5c3e0aad
Dennis Woodruff <denniswoodruffuk@gmail.com>
2023-08-09 15:01:26 +0100

    Adds generate_note_folder
    With tests and some formatting changes

--- tests/utils/test_paths.py
+++ tests/utils/test_paths.py
@@ -2,11 +2,16 @@ import pytest
 from datetime import date
 from dateutil.parser import parse
 from pathlib import Path
-from takenote.utils.paths import generate_note_path
+from takenote.utils.paths import generate_note_path, generate_note_folder
+
 
 class TestPaths:
-
     def test_note_path(self):
         week = parse("2023-08-31")
         target_path = Path("/tmp/2023-08-31-Weekly-log.md")
         assert generate_note_path(week, Path("/tmp")) == target_path
+
+    def test_note_folder(self):
+        week = parse("2023-08-31")
+        target_path = Path("/tmp/2023/08")
+        assert generate_note_folder(week, Path("/tmp")) == target_path
