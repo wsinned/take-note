ab5354e401f9946dc9aa51a8438de780539f9b85
Dennis Woodruff <denniswoodruffuk@gmail.com>
2023-08-10 09:12:46 +0100

    Adds basic arg parser
    Setting notes folder or defaulting to home folder is only arg supported.

---
+++ src/takenote/utils/args.py
@@ -0,0 +1,28 @@
+import argparse
+import pathlib
+from sys import exit
+
+
+def init_argparse() -> argparse.ArgumentParser:
+    help_text = """Create a notes file for the requested 
+        week."""
+
+    parser = argparse.ArgumentParser(usage="%(prog)s [OPTIONS]", description=help_text)
+    parser.add_argument(
+        "-v", "--version", action="version", version=f"{parser.prog} version 0.0.1"
+    )
+    parser.add_argument(
+        "--notesFolder",
+        default=pathlib.Path.home,
+        type=pathlib.Path,
+        help="The root path for the notes folder. If not supplied this defaults to the home folder.",
+    )
+
+    return parser
+
+
+def process_args(args):
+    parser = init_argparse()
+    parsed = parser.parse_args(args)
+
+    return parsed.notesFolder
---
+++ tests/utils/test_args.py
@@ -0,0 +1,11 @@
+from takenote.utils import args
+from pathlib import Path
+
+class TestArgs:
+    def test_parse_notes_folder(self):
+        notes_folder = args.process_args(['--notesFolder', '/tmp/notes'])
+        assert notes_folder == Path('/tmp/notes')
+
+    def test_default_notes_folder(self):
+        notes_folder = args.process_args([])
+        assert notes_folder == Path.home
