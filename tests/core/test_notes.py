3a2ae5ed5f9249129b18877f349dc37ea97a4273
Dennis Woodruff <denniswoodruffuk@gmail.com>
2023-08-10 08:37:18 +0100

    Convert Notes.create_file to a function.
    Keeping it simple. A class is overkill here until it's really needed.

--- tests/core/test_notes.py
+++ tests/core/test_notes.py
@@ -1,4 +1,4 @@
-from takenote.core.notes import Notes
+from takenote.core import notes
 from pathlib import Path
 from dateutil.parser import parse
 
@@ -7,7 +7,6 @@ class TestNotes:
     def test_create_file(self):
         week = parse("2023-08-31")
         homepath = Path.home()
-        notes = Notes()
         notes.create_file(week, homepath.joinpath("tmp/notes"))
         target_file = homepath.joinpath("tmp/notes/2023/08/2023-08-31-Weekly-log.md")
         assert target_file.exists()
