1a2386735aa6c3eae7d0b5359a8b9e84fbad419f
Dennis Woodruff <denniswoodruffuk@gmail.com>
2023-08-30 08:09:24 +0100

    Adds build package
    Enables packaging project as wheel built distribution and a source tar

--- shell.nix
+++ shell.nix
@@ -8,6 +8,7 @@
         pkgs.python311Packages.pip
         pkgs.python311Packages.setuptools
         pkgs.python311Packages.wheel
+        pkgs.python311Packages.build        
         pkgs.python311Packages.pytest
         pkgs.python311Packages.dateutils
     ];
